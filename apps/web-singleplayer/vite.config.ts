c1b9e1cfb9706079d3f559f6eba7e1b07b3c31c9
Emre Yasin Colakoglu <emrinho@gmail.com>
2024-02-20 19:52:36 +0300

    updated nx versions

--- apps/web-singleplayer/vite.config.ts
+++ apps/web-singleplayer/vite.config.ts
@@ -1,27 +1,30 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/web-singleplayer',
+  root: __dirname,
+  build: {
+    outDir: "../../dist/apps/web-singleplayer",
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  cacheDir: "../../node_modules/.vite/web-singleplayer",
 
   server: {
     port: 4200,
-    host: 'localhost',
+    host: "localhost",
   },
 
   preview: {
     port: 4300,
-    host: 'localhost',
+    host: "localhost",
   },
 
-  plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
-  ],
+  plugins: [react(), nxViteTsPaths()],
 
   // Uncomment this if you are using workers.
   // worker: {
@@ -33,11 +36,16 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ["default"],
+    coverage: {
+      reportsDirectory: "../../coverage/apps/web-singleplayer",
+      provider: "v8",
+    },
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest',
+      dir: "../../node_modules/.vitest",
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
 });
